8108fb8c3afe789cf4f20465315a4c8965fe36ff
Tera Byte <tubagus.aom.swk@gmail.com>
2025-08-21 11:11:51 +0700

    mergenew

---
+++ application/views/templates/bootstraps/body.php
@@ -0,0 +1,13 @@
+
+<section class="section bg-primary" style="padding-top: 450px;height:950px">
+  <div class="container">
+    <div class="row">
+      <div class="col-12 text-center">
+        <h6 class="text-white font-secondary mb-0">Click to log in the system</h6>
+        <h2 class="section-title text-white">Digital Report Transformation</h2>
+        <a  href="#loginModal" data-toggle="modal" data-target="#loginModal" class="btn btn-light">Log In</a>
+      </div>
+    </div>
+  </div>
+</section>
+
---
+++ application/views/templates/bootstraps/menu.php
@@ -0,0 +1,70 @@
+<!-- header -->
+<header class="fixed-top header">
+	<!-- top header -->
+	<!-- <div class="top-header py-2 bg-white">
+		<div class="container">
+			<div class="row no-gutters">
+
+				<div class="col-lg-4 text-center text-lg-left">
+					<a class="text-color mr-3" href="tel:+443003030266"><strong>CALL</strong> +44 300 303 0266</a>
+					<ul class="list-inline d-inline">
+						<li class="list-inline-item mx-0"><a class="d-inline-block p-2 text-color" href="https://facebook.com/themefisher/"><i class="ti-facebook"></i></a></li>
+						<li class="list-inline-item mx-0"><a class="d-inline-block p-2 text-color" href="https://twitter.com/themefisher"><i class="ti-twitter-alt"></i></a></li>
+						<li class="list-inline-item mx-0"><a class="d-inline-block p-2 text-color" href="https://github.com/themefisher"><i class="ti-github"></i></a></li>
+						<li class="list-inline-item mx-0"><a class="d-inline-block p-2 text-color" href="https://instagram.com/themefisher/"><i class="ti-instagram"></i></a></li>
+					</ul>
+				</div>
+
+				<div class="col-lg-12 text-center text-lg-right">
+					<ul class="list-inline">
+						<li class="list-inline-item"><a class="text-uppercase text-color p-sm-2 py-2 px-0 d-inline-block" href="notice.html">notice</a></li>
+						<li class="list-inline-item"><a class="text-uppercase text-color p-sm-2 py-2 px-0 d-inline-block" href="research.html">research</a></li>
+						<li class="list-inline-item"><a class="text-uppercase text-color p-sm-2 py-2 px-0 d-inline-block" href="scholarship.html">SCHOLARSHIP</a></li>
+
+						<li class="list-inline-item">
+							<a class="text-uppercase text-color p-sm-2 py-2 px-0 d-inline-block"
+								href="<?=base_url()?>uji_kompetensi" target="_blank">
+								Daftar Sertifikasi
+							</a>
+						</li>
+
+						<li class="list-inline-item">|</li>
+
+						<li class="list-inline-item">
+							<a class="text-uppercase text-color p-sm-2 py-2 px-0 d-inline-block" href="#loginModal"
+								data-toggle="modal" data-target="#loginModal">
+								Masuk
+							</a>
+						</li>
+
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div> -->
+
+
+	<!-- navbar -->
+	<div class="navigation w-100">
+		<div class="container">
+			<nav class="navbar navbar-expand-lg navbar-dark p-0">
+
+				<!-- <a class="navbar-brand" href="index.html">
+					<img src="<?php echo base_url() ?>assets/_tera_byte/images/logo.png" alt="logo">
+				</a> -->
+				<a class="navbar-brand" href="<?= base_url() ?>">
+					<img src="<?php echo base_url() ?>assets/img/jka_icon.png"
+						style="width:50px;border-radius:50%;padding:3px;" alt="tera_bytee">
+					<b>
+						<span style="color:#fff"> JASINTEK </span>
+						<span style="color:#fff"> KARYA ABADI </span>
+					</b>
+				</a>
+
+				
+				</div>
+			</nav>
+		</div>
+	</div>
+</header>
+<!-- /header -->
