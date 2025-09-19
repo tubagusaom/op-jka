aa11d03a05c574b687066ecd297afd5499c27d0c
Tera Byte <tubagus.aom.swk@gmail.com>
2025-09-19 14:42:32 +0700

    mergeupdate

--- application/views/templates/bootstraps/body.php
+++ application/views/templates/bootstraps/body.php
@@ -4,7 +4,7 @@
     <div class="row">
       <div class="col-12 text-center">
         <h6 class="text-white font-secondary mb-0">Click to log in the system</h6>
-        <h2 class="section-title text-white">Digital Report Transformation</h2>
+        <h2 class="section-title text-white">Digital inspection Transformation</h2>
         <a  href="#loginModal" data-toggle="modal" data-target="#loginModal" class="btn btn-light">Log In</a>
       </div>
     </div>
